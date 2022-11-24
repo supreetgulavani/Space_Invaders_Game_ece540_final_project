48ade8bfa3d313abd1178cbd1a5e5dd39ed8b31b
Supreet Gulavani <supreetgulavani@Supreets-Air.hsd1.or.comcast.net>
2022-11-24 00:08:48 -0800

    Add game implementation

---
+++ space_invaders_pio/include/game/space_invaders.h
@@ -0,0 +1,45 @@
+#ifndef __SPACE_INVADERS_H__
+#define __SPACE_INVADERS_H__
+
+/* Space Invaders Arcade Game */
+
+#include "commondefs.h"
+#include "psp_types.h"
+#include <stdint.h>
+
+// Defines
+#define SHIP_POSITION 320;
+#define SCORE_BOARD 620;
+
+
+// Game States
+typedef enum{
+    SPACE_INVADERS_INIT,
+    SPACE_INVADERS_WAIT_FOR_START,
+    SPACE_INVADERS_START,
+    SPACE_INVADERS_GAME_IN_PROGRESS,
+    SPACE_INVADERS_GAME_OVER,
+    SPACE_INVADERS_GAME_OVER_WAIT,
+    SPACE_INVADERS_RESTART,
+    SPACE_INVADERS_LAST = 100
+} space_invaders_t;
+
+// 2D co-ordinates
+typedef struct{
+    uint16_t x;
+    uint16_t y;
+} coordinates_2D;
+
+// Variables
+extern uint8_t game_start;
+extern wchar_t final_score[];
+
+// Functions
+void space_invaders_init(void);
+void space_invaders_task(void);
+void space_invaders_state(space_invaders_t state);
+space_invaders_t space_invaders_get_state(void);
+void ship_move(uint8_t dir);
+void write_final_score(void);
+
+#endif
---
+++ space_invaders_pio/src/game/space_invaders.c
@@ -0,0 +1,109 @@
+/* Space Invaders Arcade Game */
+
+#include "game/space_invaders.h"
+#include <stdint.h>
+
+#include "device/seven_seg.h"
+#include "device/timer.h"
+
+#include "utils/printf.h"
+
+// Global Variables
+volatile uint8_t game_render = 0;
+uint8_t game_start = 0;
+
+// Static Variables
+static coordinates_2D ship_pos;
+static coordinates_2D enemies_pos[];
+
+static uint8_t player_score;
+static s08_t enemy_shot;
+
+static space_invaders_t game_state = SPACE_INVADERS_INIT;
+
+static char msg[10];
+wchar_t final_score[20];
+
+// Static Functions
+
+// Ship position Reset
+static void ship_reset(void)
+{
+    ship_pos.x = SHIP_POSITION;
+    ship_pos.y = 430;
+}
+
+
+// Update Score
+static void game_update_score(void)
+{
+    //use hagl to write on screen
+
+    // display on seven segment
+    seven_seg_display_num(player_score, 1);
+}
+
+// Game Over Screen
+static void game_over_display_screen(void)
+{
+    wchar_t game_over[] = L"GAME OVER!";
+    
+    // use hagl?
+}
+
+// Final Score Display on Screen
+
+
+// Initialize Game
+void space_invaders_init(void)
+{
+    // Reset Ship
+    ship_reset();
+
+    // Clear 7-segment display
+    seven_seg_blank();
+
+    // Reset Score
+    player_score = 0;
+    
+    // Display Score on Screen
+    game_update_score();
+}
+
+// Game tasks
+void space_invaders_task(void)
+{
+    // State Machine
+    switch (game_state)
+    {
+    case SPACE_INVADERS_INIT:
+        space_invaders_init();
+        game_state = SPACE_INVADERS_WAIT_FOR_START;
+        break;
+    
+    case SPACE_INVADERS_WAIT_FOR_START:
+        break;
+    
+    case SPACE_INVADERS_START:
+        game_start = 1;
+        game_state = SPACE_INVADERS_GAME_IN_PROGRESS;
+        break;
+    
+    case SPACE_INVADERS_GAME_IN_PROGRESS:
+        if (game_render){
+            game_render = 0;
+            // check for collision
+            
+            // Update Score
+            player_score++;
+            game_update_score();
+            game_state = SPACE_INVADERS_GAME_OVER;
+        }
+        break;
+    case SPACE_INVADERS_GAME_OVER:
+
+
+    default:
+        break;
+    } 
+}
