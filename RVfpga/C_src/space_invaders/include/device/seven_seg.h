af26e736365d8a4bd35e6d4a1ef2017deff55a5b
Supreet Gulavani <supreetgulavani@Supreets-Air.hsd1.or.comcast.net>
2022-12-02 00:43:25 -0800

    add game files

--- RVfpga/C_src/space_invaders/include/device/seven_seg.h
+++ RVfpga/C_src/space_invaders/include/device/seven_seg.h
@@ -16,16 +16,19 @@
 
 //  7-segmenmt display values for numbers
 #define SEG_0FF     0xFF
-#define SEG_0       0xC0
-#define SEG_1       0xF9
-#define SEG_2       0xA4
-#define SEG_3       0xB0
-#define SEG_4       0x99
-#define SEG_5       0x92
-#define SEG_6       0x82
-#define SEG_7       0xF8
-#define SEG_8       0x80
-#define SEG_9       0x90
+#define SEG0       0xC0
+#define SEG1       0xF9
+#define SEG2       0xA4
+#define SEG3       0xB0
+#define SEG4       0x99
+#define SEG5       0x92
+#define SEG6       0x82
+#define SEG7       0xF8
+#define SEG8       0x80
+#define SEG9       0x90
+#define CHAR_u     0x3E
+#define CHAR_m     0x55
+#define CHAR_S     0x6D
 
 // Functions
 void seven_seg_blank(void);
